dd0aff9ac554a45ed63a23f939576f8da0dc3bc5
upthemaster <upthemaster@gmail.com>
2025-12-15 19:26:51 +0530

    Delete DAY_1 directory

--- DAY_1/arithmetic.py
+++
@@ -1,6 +0,0 @@
-def add(num1,num2):
-    print('Add is:', num1+num2)
-
-def subtract(num1,num2):
-    print('subtract is:', num1 + num2)
-    
--- DAY_1/geometry.py
+++
@@ -1,5 +0,0 @@
-def rect_area(len,br):
-    print("rect_area is:", len*br)
-
-def rect_peri(len,br):
-    print('rect_peri is:', 2*(len+br))
