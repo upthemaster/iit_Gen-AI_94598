b713b8a6456e005df8d2087ba89848ad35124658
upthemaster <upthemaster@gmail.com>
2025-12-15 19:26:10 +0530

    Delete ASSIGNMENT_02 directory

--- ASSIGNMENT_02/Challenge/news_utils.py
+++
@@ -1,7 +0,0 @@
-def print_headlines(articles, count=5):
-    print("\nTop News Headlines:\n")
-
-    for idx, article in enumerate(articles[:count], start=1):
-        print(f"{idx}. {article.get('title')}")
-        print(f"   Source: {article.get('source_id')}")
-        print()
