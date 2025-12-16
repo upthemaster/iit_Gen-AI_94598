24406b7ff31fefc232be63a7b5ee3851e7cd2e65
upthemaster <upthemaster@gmail.com>
2025-12-16 09:48:58 +0530

    Delete CLASSWORK/DAY2_CW directory

--- CLASSWORK/DAY2_CW/app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("Hello, Streamlit!")
-st.header("Welcome to GenAI Training Programme!!")
-st.write("Hello students, I hope you are enjoying tech stuff here...")
-
-if st.button("Click Me!!", type="primary"):
-    st.toast("You clicked me...")
